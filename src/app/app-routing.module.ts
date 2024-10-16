181a80a22fb6317064ed40ec025d205b3afd586e
Ubuntu <ubuntu@ip-172-31-92-248.ec2.internal>
2024-10-16 07:41:55 +0000

    Added side menu with three pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,18 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'alta',
+    loadChildren: () => import('./alta/alta.module').then( m => m.AltaPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 
