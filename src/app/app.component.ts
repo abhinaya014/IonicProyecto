c36c6d92ef12a1418e946e501f9009a3878d7181
abhinaya014 <abhinayadahal407@gmail.com>
2024-10-16 10:34:49 +0200

    Refactor side menu options to use correct URLs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inicio', url: '/folder/inicio', icon: 'mail' },
+    { title: 'Inicio', url: '/folder/folder', icon: 'mail' },
     { title: 'Alta', url: '/folder/alta', icon: 'mail' },
     { title: 'Login', url: '/folder/login', icon: 'mail' },
-
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
