8f740abb9355a3c364c631a7b8099c16d6217efd
abhinaya014 <abhinayadahal407@gmail.com>
2024-10-17 09:28:06 +0200

    Refactor app routing module to redirect root path to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,8 +12,10 @@ const routes: Routes = [
     path: 'alta',
     loadChildren: () => import('./alta/alta.module').then( m => m.AltaPageModule)
   },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
-
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  }
 ];
 
 @NgModule({
