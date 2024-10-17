36d70754097ec7547aba8c3794afa34ee3ec3e54
abhinaya014 <abhinayadahal407@gmail.com>
2024-10-17 09:21:54 +0200

    Refactor login page to add user input form and submit button

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
 
-  constructor() { }
+export class LoginPage implements OnInit {
+  
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe((response: { sucess: any; token: string; role: string; }) => {
+      if(response.sucess) {
+        
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('role', response.role);
+
+        if(response.role == 'administrador') {
+          this.router.navigate(['/alta']);
+        } else if (response.role == 'alumno') { 
+          this.router.navigate(['/inicio']);
+          
+        } else {
+          console.log('Credenciales incorrectas');
+        }
+
+        
+      }
+    })
+  }
 
   ngOnInit() {
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = 'http://34.226.133.9:8000/api/user';
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string,password:string): Observable<any>
+  {
+    return this.http.put(this.apiUrl, {email , password});
+  }
 }
