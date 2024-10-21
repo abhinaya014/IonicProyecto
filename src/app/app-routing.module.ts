f6df00a88344fd9cd53bb3e860fce10d0f3acd38
abhinaya014 <abhinayadahal407@gmail.com>
2024-10-21 16:40:44 +0200

    asdas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,16 +13,22 @@ const routes: Routes = [
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
   {
-    path: 'admin/:id',  // Ruta para la página de admin con ID del curso
+    path: 'admin/:id', 
     loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule),
     canActivate: [AuthGuard],
     data: { role: 'administrador' }
   },
   {
-    path: 'alumno/:id',  // Ruta para la página de alumno con ID del curso
+    path: 'alumno/:id',  
     loadChildren: () => import('./alumno/alumno.module').then( m => m.AlumnoPageModule),
     canActivate: [AuthGuard],
     data: { role: 'alumno' }
+  },
+  {
+    path: 'alta',
+    loadChildren: () => import('./alta/alta.module').then(m => m.AltaPageModule),
+    canActivate: [AuthGuard],
+    data: { role: 'administrador' }  
   },
   {
     path: '',
